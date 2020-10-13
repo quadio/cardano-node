fa030e93f04114e25a180dc04abd9d2702e92bc6
Luke Nadur <19835357+intricate@users.noreply.github.com>
2020-10-13 13:25:35 -0400

    Add support for Bech32 serialisation of keys in Shelley address CLI

--- cardano-cli/src/Cardano/CLI/Shelley/Run/Address.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Run/Address.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 
 module Cardano.CLI.Shelley.Run.Address
@@ -17,13 +18,13 @@ import qualified Data.Text.IO as Text
 
 import           Control.Monad.Trans.Except.Extra (firstExceptT, handleIOExceptT, left, newExceptT)
 
-import           Cardano.Api.TextView (TextViewDescription (..))
 import           Cardano.Api.Typed
 
-import           Cardano.CLI.Shelley.Key (InputDecodeError, VerificationKeyOrFile,
-                     VerificationKeyTextOrFile, VerificationKeyTextOrFileError (..),
-                     readVerificationKeyOrFile, readVerificationKeyTextOrFileAnyOf,
-                     renderVerificationKeyTextOrFileError)
+import           Cardano.CLI.Shelley.Key (InputDecodeError, OutputDirection (..),
+                     VerificationKeyOrFile, VerificationKeyTextOrFile,
+                     VerificationKeyTextOrFileError (..), readVerificationKeyOrFile,
+                     readVerificationKeyTextOrFileAnyOf, renderVerificationKeyTextOrFileError,
+                     serialiseInputToBech32AndWrite)
 import           Cardano.CLI.Shelley.Parsers (AddressCmd (..), AddressKeyType (..), OutputFile (..))
 import           Cardano.CLI.Shelley.Run.Address.Info (ShelleyAddressInfoError, runAddressInfo)
 import           Cardano.CLI.Types
@@ -75,14 +76,14 @@ runAddressKeyGen kt (VerificationKeyFile vkeyPath) (SigningKeyFile skeyPath) =
       let vkey = getVerificationKey skey
       firstExceptT ShelleyAddressCmdWriteFileError
         . newExceptT
-        $ writeFileTextEnvelope skeyPath (Just skeyDesc) skey
+        $ serialiseInputToBech32AndWrite
+            (OutputDirectionFile skeyPath)
+            skey
       firstExceptT ShelleyAddressCmdWriteFileError
         . newExceptT
-        $ writeFileTextEnvelope vkeyPath (Just vkeyDesc) vkey
-
-    skeyDesc, vkeyDesc :: TextViewDescription
-    skeyDesc = TextViewDescription "Payment Signing Key"
-    vkeyDesc = TextViewDescription "Payment Verification Key"
+        $ serialiseInputToBech32AndWrite
+            (OutputDirectionFile vkeyPath)
+            vkey
 
 
 runAddressKeyHash :: VerificationKeyTextOrFile
