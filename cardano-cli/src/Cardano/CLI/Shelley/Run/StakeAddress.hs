e8624ab484821370f0fa8d18fd7f750252f597fe
Luke Nadur <19835357+intricate@users.noreply.github.com>
2020-10-13 13:43:32 -0400

    Add support for Bech32 serialisation of keys in Shelley stake address CLI

--- cardano-cli/src/Cardano/CLI/Shelley/Run/StakeAddress.hs
+++ cardano-cli/src/Cardano/CLI/Shelley/Run/StakeAddress.hs
@@ -15,9 +15,9 @@ import           Control.Monad.Trans.Except.Extra (firstExceptT, newExceptT)
 import           Cardano.Api.TextView (TextViewDescription (..))
 import           Cardano.Api.Typed
 
-import           Cardano.CLI.Shelley.Key (InputDecodeError, VerificationKeyOrFile,
-                     VerificationKeyOrHashOrFile, readVerificationKeyOrFile,
-                     readVerificationKeyOrHashOrFile)
+import           Cardano.CLI.Shelley.Key (InputDecodeError, OutputDirection (..),
+                     VerificationKeyOrFile, VerificationKeyOrHashOrFile, readVerificationKeyOrFile,
+                     readVerificationKeyOrHashOrFile, serialiseInputToBech32AndWrite)
 import           Cardano.CLI.Shelley.Parsers
 import           Cardano.CLI.Types
 
@@ -63,14 +63,14 @@ runStakeAddressKeyGen (VerificationKeyFile vkFp) (SigningKeyFile skFp) = do
     let vkey = getVerificationKey skey
     firstExceptT ShelleyStakeAddressCmdWriteFileError
       . newExceptT
-      $ writeFileTextEnvelope skFp (Just skeyDesc) skey
+      $ serialiseInputToBech32AndWrite
+          (OutputDirectionFile skFp)
+          skey
     firstExceptT ShelleyStakeAddressCmdWriteFileError
       . newExceptT
-      $ writeFileTextEnvelope vkFp (Just vkeyDesc) vkey
-  where
-    skeyDesc, vkeyDesc :: TextViewDescription
-    skeyDesc = TextViewDescription "Stake Signing Key"
-    vkeyDesc = TextViewDescription "Stake Verification Key"
+      $ serialiseInputToBech32AndWrite
+          (OutputDirectionFile vkFp)
+          vkey
 
 runStakeAddressKeyHash
   :: VerificationKeyOrFile StakeKey
